d11cca7adfdb16caa6cd379b9ced6172f1703c97
Aki Guanzon <83754370+akiguanzon@users.noreply.github.com>
2023-02-13 10:31:57 +0800

    Add files via upload

---
+++ controller/controllerpatient.js
@@ -0,0 +1,57 @@
+var User = require('../model/user');
+var Patient = require('../model/patient');
+var types = ['Surgical', 'Purely Medical', 'Checkup'];
+
+
+
+const controllerPatient = {
+
+    //Get page to create a new patient in the DB
+    createPatient: async (req, res) => {
+        res.render('patient/newPatient', { types });
+    },
+
+    //Post the new patient data into the DB
+    addPatient: async (req, res) => {
+        if (req.session.username) {
+            var data = req.body;
+            data.creator = req.session.username;
+            var newData = new Patient(data);
+            await newData.save()
+                .then(async () => {
+                    res.redirect('/');
+                })
+                .catch((err) => {
+                    message = err;
+                    res.redirect('/patient/new');
+                })
+
+        }
+        else {
+            message = 'Login to proceed.';
+            console.log('Login to proceed.');
+            res.redirect('/user/login');
+        }
+    },
+    viewPatient: async (req, res) => {
+        const patientId = req.params.patientId;
+
+        res.render('patient/patientRecord', { types });
+        Post.find({ _id: patientId }, function (err, result) {
+            if (err) {
+                console.log(err);
+            } else {
+                res.render('record', {
+                    post: result[0]
+                });
+            }
+        });
+    },
+    searchPatients: async (req, res) => {
+        var { q } = req.query;
+        var searchPatients = await Patient.find({ $text: { $search: q } }).sort({ 'date': -1 });
+        res.render('patient/searchPatient', { q, searchPatients });
+    },
+}
+
+module.exports = controllerPatient;
---
+++ routes/route.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+
+mongoose.connect("mongodb://127.0.0.1:27017/blogDB", { useNewUrlParser: true });
+
+const controllerUser = require('../controller/controlleruser.js');
+const controllerPatient = require('../controller/controllerpatient.js');
+const controllerHome = require('../controller/controllerhome.js');
+
+//Home page section
+app.get('/', controllerHome.goHome);
+//app.get('/about', controllerhome.goAbout)
+
+//Login and signup section
+app.get('/user/login', controllerUser.login);
+app.post('/user', controllerUser.checkLogin);
+app.get('/user/signup', controllerUser.signup);
+app.post('/user/signup', controllerUser.addUser);
+app.get('/user/:username', controllerUser.userPage);
+
+//Records section
+app.get('/patient/new', controllerPatient.createPatient);
+app.post('/patient', controllerPatient.addPatient);
+app.get('/patient/:patientId', controllerPatient.viewPatient);
+app.get('/patients/search', controllerPatient.searchPatients);
+
+module.exports = app;
