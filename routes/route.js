6fd93b712f2364eeb6f7ac295b3d39b2b86b2826
Stephanie Joy Relles <stephanie_susada@dlsu.edu.ph>
2023-02-27 10:08:37 +0800

    updated

--- routes/route.js
+++ routes/route.js
@@ -23,4 +23,9 @@ app.post('/patient', controllerPatient.addPatient);
 app.get('/patient/:patientId', controllerPatient.viewPatient);
 app.get('/patients/search', controllerPatient.searchPatients);
 
+// Search
+app.get('/patients/patient/:patientId', controllerPatient.viewPatient); 
+app.get('/patient/edit/:patientId', controllerPatient.editPatient);
+app.post('/patient/modify/:patientId', controllerPatient.modifyPatient);
+
 module.exports = app;
